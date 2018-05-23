b053c6445bffdcda51fb55ad4d6ea8201fffc2e1
canye <canye.wu@jyblife.com>
2018-05-23 20:55:38 +0800

    feat: 优化显示

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,22 +5,42 @@
 const path = require('path');
 const fse = require('fs-extra');
 
-const root = path.join(__dirname, '..');
-
 module.exports = (app) => {
   class HomeController extends app.Controller {
     * index() {
       const ctx = this.ctx;
-      const publicPath = path.join(root, 'public/book');
+      const publicPath = path.join(app.baseDir, 'app/public/book');
       const title = '加油宝文档服务';
 
       try {
-        const dirs = (yield fse.readdir(publicPath) || []).filter((dir) => {
+        const docs = (yield fse.readdir(publicPath) || []).filter((dir) => {
           const stats = fse.statSync(path.join(publicPath, dir));
           return stats.isDirectory();
-        }).map(dir => `book/${dir}`);
+        }).map((dir) => {
+          const docsFile = path.join(publicPath, dir, 'docs_server.json');
+          let docsContent = '';
+          let docsJson = {};
 
-        yield ctx.render('home.html', { title, dirs });
+          if (fileExists(docsFile)) {
+            docsContent = fse.readFileSync(docsFile);
+          }
+
+          if (docsContent) {
+            try {
+              docsJson = JSON.parse(docsContent);
+            } catch (e) {
+              docsJson = {};
+            }
+          }
+
+          return {
+            title: docsJson.title || dir,
+            desc: docsJson.desc || dir,
+            dir: `book/${dir}`
+          };
+        });
+
+        yield ctx.render('home.html', { title, docs });
       } catch (e) {
         ctx.body = title;
       }
@@ -28,3 +48,11 @@ module.exports = (app) => {
   }
   return HomeController;
 };
+
+function fileExists(filePath) {
+  try {
+    return fse.statSync(filePath).isFile();
+  } catch (e) {
+    return false;
+  }
+}
